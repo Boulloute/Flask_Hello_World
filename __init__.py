b18ebf8f0a34fce8fb9fbfe5599027f2ae6eb4a5
Boulloute <109070193+Boulloute@users.noreply.github.com>
2024-04-11 12:23:12 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,9 +17,8 @@ def exercices():
 @app.route('/calcul_carre/<int:val_user>')
 def carre(val_user):
     return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
-@app.route('/somme/<int:val_user>')
+@app.route('/somme/<int:val_user>/<int:val_user>')
 def somme(val_user + val_user):
-  input(val_user)
   input(val_user)
   somme = val_user + val_user
   if somme % 2 = 0 :
